ed57d8c6dcdbd601d84c7b545371b032b8e4db9e
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2026-04-28 16:38:16 +0900

    <Player/NPC/feat> Player同士が接触した際、すり抜けるように改修

--- GameTemplate/Game/Src/Actor/Character/Player/Component/PlayerGenerator.cpp
+++ GameTemplate/Game/Src/Actor/Character/Player/Component/PlayerGenerator.cpp
@@ -26,16 +26,26 @@ namespace nsApp
 
 	void PlayerGenerator::SpawnPlayers(const std::vector<PlayerSpawnData>& spawnDataList)
 	{
+		/* */
+		std::vector<nsActor::Player*> spawnedPlayers;
+
 		/* リストの数だけプレイアブルキャラを生成する。*/
 		for (const auto& data : spawnDataList)
 		{
-			/* */
+			/* 生成するキャラを数え、武器の種類と生成名をセット。*/
 			if (m_characterFactory.count(data.weaponType) > 0)
 				m_spawnPlayer = m_characterFactory[data.weaponType](data.playerName);
 
 			/* 生成に成功した場合。*/
 			if (m_spawnPlayer != nullptr)
+			{
+				/* */
 				m_spawnPlayer->InitializeSpawnData(data);
+
+				/* 生成したプレイヤーをリストに保存する。*/
+				spawnedPlayers.push_back(m_spawnPlayer);
+			}
 		}
+
 	}
 }
