332ace475ed13fe73fdf2d7168f3125a0eb326c5
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2026-05-28 11:32:25 +0900

    <refactor> Gun/Magicクラス群をプール化

--- GameTemplate/Game/Src/Actor/Magic/Factory/MagicFactory.cpp
+++ GameTemplate/Game/Src/Actor/Magic/Factory/MagicFactory.cpp
@@ -3,9 +3,35 @@
 #include "Src/Actor/Magic/Parameter/MagicParameterTable.h"
 #include "Src/Actor/Magic/MagicProjectotile.h"
 
+namespace
+{
+	const char* GetMagicObjectName(nsApp::nsActor::MagicType type)
+	{
+		using nsApp::nsActor::MagicType;
+
+		switch (type)
+		{
+		case MagicType::enNormalMagic:
+			return "MagicObject_Normal";
+
+		case MagicType::enRushMagic:
+			return "MagicObject_Rush";
+
+		case MagicType::enAirMagic:
+			return "MagicObject_Air";
+
+		case MagicType::enChargeMagic:
+			return "MagicObject_Charge";
+
+		default:
+			return "MagicObject_Normal";
+		}
+	}
+}
+
 namespace nsApp
 {
-	void MagicFactory::CreateMagicObject(nsActor::MagicType type, const Vector3& basePosition, const Vector3& forward, nsActor::ICharacter* target)
+	void MagicFactory::CreateMagicObject(MAGICTYPE type,const Vector3& basePosition,const Vector3& forward,CHARACTER* target,EFFECT* effectList)
 	{
 		/* テーブルから魔法の種類の情報を取りだす。*/
 		const auto& magicParameter = MagicParameterTable::GetParameter(type);
@@ -14,19 +40,34 @@ namespace nsApp
 		Vector3 localForwardPosition = forward;
 		localForwardPosition.Normalize();
 
-		/* 生成座標を正規化し、補正する。*/
+		/* 生成座標を補正する。*/
 		Vector3 localSpawnPosition = basePosition;
 		localSpawnPosition.y += magicParameter.spawnOffsetY;
 		localSpawnPosition += localForwardPosition * magicParameter.forwardOffset;
 
-		/* 魔法オブジェクトクラスを生成し、初期化する。*/
-		auto* createMagic = NewGO<nsActor::MagicProjectotile>(0, "MagicObject");
+		/* 未使用の魔法弾を探す。*/
+		const char* objectName = GetMagicObjectName(type);
+
+		nsActor::MagicProjectotile* createMagic = nullptr;
+
+		const auto& magics = FindGOs<nsActor::MagicProjectotile>(objectName);
+		for (auto* pooledMagic : magics)
+		{
+			if (pooledMagic != nullptr && !pooledMagic->IsInUse())
+			{
+				createMagic = pooledMagic;
+				break;
+			}
+		}
+
+		if (createMagic == nullptr)
+			createMagic = NewGO<nsActor::MagicProjectotile>(0, objectName);
+
 		if (createMagic)
 		{
-			/* 初期化。*/
 			createMagic->Initialize(type, localSpawnPosition, localForwardPosition, magicParameter);
+			createMagic->SetEffectList(effectList);
 
-			/* 追従目標が設定されているならば、設定する。*/
 			if (target)
 				createMagic->SetTarget(target);
 		}
